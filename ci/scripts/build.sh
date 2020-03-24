27c0437f3e5bc72a25ce7c9d28efa54e06f9adcf
CarlHembrough <carlhuk@gmail.com>
2020-03-24 11:38:06 +0000

    Update CI scripts for migration to Go modules
    - update LICENSE.md with current year.

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -2,9 +2,7 @@
 
 cwd=$(pwd)
 
-export GOPATH=$cwd/go
-
-pushd $GOPATH/src/github.com/ONSdigital/dp-search-api
+pushd $cwd/dp-search-api
   make build && mv build/$(go env GOOS)-$(go env GOARCH)/* $cwd/build
   cp Dockerfile.concourse $cwd/build
 popd
--- ci/scripts/unit.sh
+++ ci/scripts/unit.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -eux
 
-export GOPATH=$(pwd)/go
 
-pushd $GOPATH/src/github.com/ONSdigital/dp-search-api
+pushd $cwd/dp-search-api
   make test
 popd
