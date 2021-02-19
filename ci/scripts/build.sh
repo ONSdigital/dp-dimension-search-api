673622be3613fff68c98f594bbeb5d02fb2c8ec1
nshumoogum <nathan.shumoogum@methods.co.uk>
2021-02-19 11:28:01 +0000

    Update ci build script to work with new make build command

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -3,6 +3,6 @@
 cwd=$(pwd)
 
 pushd $cwd/dp-dimension-search-api
-  make build && mv build/$(go env GOOS)-$(go env GOARCH)/* $cwd/build
+  make build && mv build/dp-dimension-search-api $cwd/build
   cp Dockerfile.concourse $cwd/build
 popd
